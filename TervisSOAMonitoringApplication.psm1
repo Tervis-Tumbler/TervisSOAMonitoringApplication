96df82f7d34ea2005a8444ccf1302ef2bc93b303
ChrisMagnuson <hourglasssand@gmail.com>
2018-05-14 10:54:13 -0400

    Remove uneeded functions, refactor to use code in PowerShellApplicaiton

--- TervisSOAMonitoringApplication.psm1
+++ TervisSOAMonitoringApplication.psm1
@@ -7,70 +7,8 @@ $SOAEnvironments = [PSCustomObject]@{
     JobsThatShouldBeDisabled = "WarrantyOrderJob","WOMZRJob","ImageIntJob"
 }
 
-function Invoke-TervosOracleSOAJobMonitoringApplication {
-    param (
-        $SOASchedulerURL = $env:SOASchedulerURL,
-        $EmailTo = $env:EmailTo,
-        $EmailFrom = $env:EmailFrom
-    )
-    Invoke-TervisOracleSOAJobMonitoring  @PSBoundParameters
-}
-
-function Invoke-TervosOracleSOAJobMonitoringApplicationJsonParam {
-    $ConfigurationParameters = Get-Content -Path $ModulePath/Parameters.json |
-    ConvertFrom-Json |
-    ConvertTo-HashTable
-
-    Invoke-TervisOracleSOAJobMonitoring  @ConfigurationParameters
-}
-
 function Invoke-TervisSOAMonitoringApplicationDockerBuild {
-    $BuildDirectory = "$($env:TMPDIR)SOAMonitorngDocker"
-    New-Item -ItemType Directory -Path $BuildDirectory -ErrorAction SilentlyContinue
-    Invoke-PSDepend -Force -Install -InputObject @{
-        PSDependOptions = @{
-            Target = $BuildDirectory
-        }
-    
-        'Tervis-Tumbler/TervisMailMessage' = 'master'
-        'Tervis-Tumbler/TervisOracleSOASuite' = 'master'
-        'Tervis-Tumbler/TervisSOAMonitoringApplication' = 'master'
-    }
-
-    Push-Location -Path $BuildDirectory
-
-@"
-**/.git
-**/.vscode
-"@ | Out-File -Encoding ascii -FilePath .dockerignore -Force
-
-@"
-FROM microsoft/powershell
-ENV TZ=America/New_York
-RUN echo `$TZ > /etc/timezone && \
-    apt-get update && apt-get install -y tzdata && \
-    rm /etc/localtime && \
-    ln -snf /usr/share/zoneinfo/`$TZ /etc/localtime && \
-    dpkg-reconfigure -f noninteractive tzdata && \
-    apt-get clean
-COPY . /usr/local/share/powershell/Modules
-#ENTRYPOINT ["pwsh", "-Command", "Invoke-TervisOracleSOAJobMonitoring " ]
-ENTRYPOINT ["pwsh"]
-"@ | Out-File -Encoding ascii -FilePath .\Dockerfile -Force
-
-    docker build --no-cache -t tervissoamonitoringapplication .
-
-    Pop-Location
-
-    Remove-Item -Path $BuildDirectory -Recurse -Force
-}
-
-function Remove-DockerContainerAllOff {
-    docker rm $(docker ps -a -q)
-}
-
-function Get-DockerContainer {
-    docker ps -a
+    Invoke-PowerShellApplicationDockerBuild -ModuleName TervisSOAMonitoringApplication -DependentTervisModuleNames "TervisMailMessage","TervisOracleSOASuite" -CommandsString "Invoke-TervisOracleSOAJobMonitoring"
 }
 
 function Invoke-TervisSOAMonitoringApplicationDockerRun {
@@ -82,13 +20,41 @@ function Invoke-TervisSOAMonitoringApplicationDockerRun {
 @"
 docker run --tty --interactive --env SOASchedulerURL="$SOASchedulerURL" --env EmailTo="$EmailTo" --env EmailFrom="$EmailFrom" --name soamon tervissoamonitoringapplication
 "@
-
-
-    #docker run --tty --interactive --env TZ=America/New_York --name soamon --volume /Users/chrismagnuson/.local/share/powershell/Modules/:/usr/local/share/powershell/Modules microsoft/powershell
-    #docker run --tty --interactive --env TZ=America/New_York --name soamon --volume /Users/chrismagnuson/.local/share/powershell/Modules/:/usr/local/share/powershell/Modules testing
 }
 
-function Func {
+function Invoke-InstallTervisSAMonitoringApplication {
+    param (
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ComputerName
+    )
+    process {
+        $OFSBackup = $OFS
+        $OFS = ""
+
+        $ScheduledScriptCommandsString = @"
+$(
+    foreach ($SOAEnvironment in $SOAEnvironments) {
+        "Invoke-TervisOracleSOAJobMonitoring -SOASchedulerURL $($SOAEnvironment.SOASchedulerURL) -NotificationEmail $($SOAEnvironment.NotificationEmail) -EnvironmentName $($SOAEnvironment.Name) -JobsThatShouldBeDisabled $($SOAEnvironment.JobsThatShouldBeDisabled -join ",")"
+    }
+)
+"@        
+        $OFS = $OFSBackup
+        
+        $ScheduledTasksCredential = New-Object System.Management.Automation.PSCredential ("system", (new-object System.Security.SecureString))
+
+        $InstallPowerShellApplicationParameters = @{
+            ModuleName = "TervisSOAMonitoringApplication"
+            DependentTervisModuleNames = "TervisMailMessage","TervisOracleSOASuite"
+            ScheduledScriptCommandsString = $ScheduledScriptCommandsString
+            ScheduledTasksCredential = $ScheduledTasksCredential
+            SchduledTaskName = "Invoke-TervisOracleSOAJobMonitoring"
+            RepetitionIntervalName = "EveryDayEvery15Minutes"
+        }
+    
+        Install-PowerShellApplication -ComputerName $ComputerName @InstallPowerShellApplicationParameters
+    }
+}
+
+function Test-TervisSOAMonitoringApplication {
     Invoke-TervisSOAMonitoringApplicationDockerRun -SOASchedulerURL http://soaweblogic.production.tervis.prv:7201/SOAScheduler/soaschedulerservlet?action=read -EmailTo cmagnuson@tervis.com -EmailFrom cmagnuson@tervis.com
     Invoke-TervisOracleSOAJobMonitoring  -SOASchedulerURL http://soaweblogic.production.tervis.prv:7201/SOAScheduler/soaschedulerservlet?action=read -EmailTo cmagnuson@tervis.com -EmailFrom cmagnuson@tervis.com
     Invoke-InstallTervisSAMonitoringApplication -SOASchedulerURL http://soaweblogic.production.tervis.prv:7201/SOAScheduler/soaschedulerservlet?action=read -EmailTo cmagnuson@tervis.com -EmailFrom cmagnuson@tervis.com -ComputerName inf-tasks01
@@ -105,51 +71,4 @@ function Func {
     kubectl proxy
     kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
     http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
-}
-
-function Invoke-InstallTervisSAMonitoringApplication {
-    param (
-        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ComputerName
-    )
-    process {
-        $ProgramData = "C:\ProgramData"
-        $SOAMonitoringDirectoryLocal = "$ProgramData\Tervis\SOAMonitoring"
-        $SOAMonitoringDirectoryRemote = $SOAMonitoringDirectoryLocal | ConvertTo-RemotePath -ComputerName $ComputerName
-        Remove-Item -Path $SOAMonitoringDirectoryRemote -ErrorAction SilentlyContinue -Recurse -Force
-        New-Item -ItemType Directory -Path $SOAMonitoringDirectoryRemote -ErrorAction SilentlyContinue
-        Invoke-PSDepend -Force -Install -InputObject @{
-            PSDependOptions = @{
-                Target = $SOAMonitoringDirectoryRemote
-            }
-        
-            'Tervis-Tumbler/TervisMailMessage' = 'master'
-            'Tervis-Tumbler/TervisOracleSOASuite' = 'master'
-            'Tervis-Tumbler/TervisSOAMonitoringApplication' = 'master'
-        }
-        $OFSBackup = $OFS
-        $OFS = ""
-@"
-Get-ChildItem -Path $SOAMonitoringDirectoryLocal -Directory | 
-ForEach-Object {
-    Import-Module -Name `$_.FullName -Force
-}
-
-$(
-    foreach ($SOAEnvironment in $SOAEnvironments) {
-        "Invoke-TervisOracleSOAJobMonitoring -SOASchedulerURL $($SOAEnvironment.SOASchedulerURL) -NotificationEmail $($SOAEnvironment.NotificationEmail) -EnvironmentName $($SOAEnvironment.Name) -JobsThatShouldBeDisabled $($SOAEnvironment.JobsThatShouldBeDisabled -join ",")"
-    }
-)
-"@ |
-        Out-File -FilePath $SOAMonitoringDirectoryRemote\Script.ps1
-        
-        $OFS = $OFSBackup
-
-        $ScheduledTasksCredential = New-Object System.Management.Automation.PSCredential ("system", (new-object System.Security.SecureString))
-
-        Install-PowerShellApplicationScheduledTask -PathToScriptForScheduledTask $SOAMonitoringDirectoryLocal\Script.ps1 `
-            -TaskName "Invoke-TervisOracleSOAJobMonitoring" `
-            -Credential $ScheduledTasksCredential `
-            -RepetitionInterval EveryDayEvery15Minutes `
-            -ComputerName $ComputerName
-    }
 }
